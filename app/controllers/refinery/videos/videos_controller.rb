47e05a663d14e4f264daaad03d069555f032e3d2
daruna <f0rever@mail.ru>
2015-02-10 12:01:57 +0400

    wip10

--- app/controllers/refinery/videos/videos_controller.rb
+++ app/controllers/refinery/videos/videos_controller.rb
@@ -18,8 +18,7 @@ module Refinery
       end
 
       def search
-        video_ids = Video.find_with_index(params[:query].to_ascii,{},{ids_only: true})
-        @videos = Video.where(id: video_ids).paginate(page: params[:page], per_page: 12)
+        @videos = Video.with_query(params[:query].to_ascii).paginate(page: params[:page], per_page: 12)
         respond_to do |format|
           format.js {render 'refinery/videos/videos/index', videos: @videos}
           format.html {render 'refinery/videos/videos/index', videos: @videos}
